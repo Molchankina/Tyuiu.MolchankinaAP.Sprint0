74cf4545281b960aa0b304f2604b1e9ae5cc820d
Ангелина Молчанкина <molchankinaap@outlook.com>
2024-09-23 19:56:50 +0500

    Task4

---
+++ Tyuiu.MolchankinaAP.Sprint0.Task4.V0.Lib/DataServise.cs
@@ -0,0 +1,23 @@
+﻿namespace Tyuiu.MolchankinaAP.Sprint0.Task4.V0.Lib
+{
+    public class DataServise
+    {
+        //Пример линейной функции
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Subtraction(int a,int b) 
+        {
+            return a - b; 
+        }
+        public static int Multiplication(int a,int b) 
+        {
+            return a * b; 
+        }
+        public static int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Tyuiu.MolchankinaAP.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MolchankinaAP.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.MolchankinaAP.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataServise.Addition(1,5));
+            Console.WriteLine(DataServise.Subtraction(15, 5));
+            Console.WriteLine(DataServise.Multiplication(10, 5));
+            Console.WriteLine(DataServise.Division(5, 5));
+
+            Console.ReadLine();
+        }
+    }
+}
