9abac0170ad278140dd7533c7db40ed6a9b91da2
Ангелина Молчанкина <molchankinaap@outlook.com>
2024-10-01 01:23:28 +0500

    Task5

---
+++ Tyuiu.MolchankinaAP.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+using Tyuiu.MolchankinaAP.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.MolchankinaAP.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataServise.Addition(5, 5));
+        }
+
+        [TestMethod]
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataServise.Subtraction(10, 5));
+        }
+
+        [TestMethod]
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataServise.Multiplication(10, 5));
+        }
+
+        [TestMethod]
+        public void CheckedDivisionValid() 
+        {
+            Assert.AreEqual(3, DataServise.Division(9, 3));
+        }
+    }
+}
---
+++ Tyuiu.MolchankinaAP.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.MolchankinaAP.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.MolchankinaAP.Sprint0.Task5.V0;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("A + B = " + DataServise.Addition(1, 5));
+        Console.WriteLine("A - B = " + DataServise.Subtraction(15, 5));
+        Console.WriteLine("A * B = " + DataServise.Multiplication(10, 5));
+        Console.WriteLine("A / B = " + DataServise.Division(9, 3));
+        Console.ReadLine();
+    }
+}
